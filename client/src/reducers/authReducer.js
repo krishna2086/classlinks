d0f8e563149be1caf03709a75277deaa23e6c20d
krishna <2018bcs086@sggs.ac.in>
2020-09-01 15:52:44 +0530

    make it better

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -11,7 +11,7 @@ const initialState = {
 };
 
 export default function(state = initialState, action) {
- // console.log(action.payload)
+  console.log(action.payload)
 
   switch (action.type) {
     case SET_CURRENT_USER:
