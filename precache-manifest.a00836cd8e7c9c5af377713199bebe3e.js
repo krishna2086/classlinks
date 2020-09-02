ce8ad40f9822d1becbec9775c0607dac06b6ff11
krishna2086 <2018bcs086@sggs.ac.in>
2020-09-02 13:52:47 +0530

    Updates

---
+++ precache-manifest.a00836cd8e7c9c5af377713199bebe3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "929cd73b0cd2b3021bde",
+    "url": "/classlinks/static/css/main.cebc3e7a.chunk.css"
+  },
+  {
+    "revision": "929cd73b0cd2b3021bde",
+    "url": "/classlinks/static/js/main.929cd73b.chunk.js"
+  },
+  {
+    "revision": "e092d9e5ef1616ebf4a0",
+    "url": "/classlinks/static/js/1.e092d9e5.chunk.js"
+  },
+  {
+    "revision": "ee10f3808f6533b02e6c",
+    "url": "/classlinks/static/js/runtime~main.ee10f380.js"
+  },
+  {
+    "revision": "6e78ca9338c9c078386487d20ed77e65",
+    "url": "/classlinks/index.html"
+  }
+];
